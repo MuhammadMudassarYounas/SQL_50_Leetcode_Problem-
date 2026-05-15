116941ba22d4f456d81676ea6c39e88a4e4f9b7d
MuhammadMudassarYounas <151513066+MuhammadMudassarYounas@users.noreply.github.com>
2026-05-15 21:10:24 +0500

    269 ms Beats 23.99%

---
+++ 1661-Average-Time-Process-per-Machine.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+Select a1. machine_id,round(avg(a2.timestamp - a1.timestamp),3) as processing_time 
+from Activity a1
+join Activity a2 
+on a1.machine_id = a2.machine_id
+and  a1.process_id = a2.process_id 
+-- and a1.timestamp <a2.timestamp   "this is run only 0,1 form but not work in 1,0 form"
+and a1.activity_type="start"
+and a2.activity_type="end"
+group by a1.machine_id
